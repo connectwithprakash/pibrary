8eb3a49e817d3d03f5848ef0d08cf480a5b36b99
Prakash Chaudhary <connectwithprakash@gmail.com>
2024-05-20 20:59:16 -0500

    [Chore]: Dependencies update.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="pibrary",  # Replace with your own username
     version="0.1.0-aplha.1",
     author="Prakash Chaudhary",
     author_email="connectwithprakash@gmail.com",
